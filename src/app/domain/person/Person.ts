68779909abe93d09cf21c01c05c80dde4b3767c3
Serhii Repinskyi <115186821+serhiirepinskyi@users.noreply.github.com>
2024-08-15 22:35:25 +0300

    Feat: add middlewares – HTTPRequestLogger, HTTPResponseLogger

--- src/app/domain/person/Person.ts
+++ src/app/domain/person/Person.ts
@@ -1,4 +1,11 @@
-import { JsonController, Get, Post, Body, Param } from "routing-controllers";
+import {
+  JsonController,
+  Get,
+  Post,
+  Body,
+  Param,
+  UseAfter,
+} from "routing-controllers";
 import { validate } from "class-validator";
 
 import { IPerson } from "./Person.types";
@@ -6,12 +13,14 @@ import { CreatePerson } from "./CreatePerson.dto";
 
 import { ApiResponse } from "helpers/ApiResponse";
 import { ApiError } from "helpers/ApiError";
+import { HTTPResponseLogger } from "app/middlewares/HTTPResponseLogger";
 
 const storeData: IPerson[] = [];
 
 @JsonController("/person")
 export default class Person {
   @Get()
+  @UseAfter(HTTPResponseLogger)
   async getAll() {
     return new ApiResponse(true, storeData);
   }
